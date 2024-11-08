eb7790b2cf8d9173ff69df1d20caadc618a06f70
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-08 16:34:12 -0300

    Passando o route params para as rotas

--- src/server.js
+++ src/server.js
@@ -12,6 +12,9 @@ const server = http.createServer(async (req, res) => {
 
   if (route) {
     const routeParams = req.url.match(route.path)
+    const { ...params } = routeParams.groups
+
+    req.params = params
 
     return route.handler(req, res)
   }
