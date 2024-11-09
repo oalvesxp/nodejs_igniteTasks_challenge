8baabdb3ecb0abde9acf3cf7c274050b57dcc098
Osvaldo A. Neto <oalves.neto@outlook.com>
2024-11-09 12:21:13 -0300

    Extraindo query params

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http'
 import { routes } from './routes.js'
 import { json } from './middlewares/json.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
@@ -12,9 +13,10 @@ const server = http.createServer(async (req, res) => {
 
   if (route) {
     const routeParams = req.url.match(route.path)
-    // const { ...params } = routeParams.groups
+    const { query, ...params } = routeParams.groups
 
-    req.params = routeParams.groups
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
 
     return route.handler(req, res)
   }
